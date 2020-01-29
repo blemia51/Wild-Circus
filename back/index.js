3472da7221ccab969d2692aeeaa68016a3f02ed2
Herve Bourelle <herve.bourelle@gmail.com>
2020-01-29 16:03:33 +0100

    create database and use passeport strategy

--- back/index.js
+++ back/index.js
@@ -9,7 +9,6 @@ const LocalStrategy = require('passport-local');
 const jwt = require('jsonwebtoken'); // npm install jsonwebtoken
 const JwtStrategy = require('passport-jwt').Strategy; // npm install passport-jwt
 const ExtractJwt = require('passport-jwt').ExtractJwt; // npm install passport-local
-const verifyToken = require('./verifyToken');
 const key = require('./key');
 const bcrypt = require('bcrypt'); // npm install bcrypt
 const port = 8000;
@@ -21,6 +20,53 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cors());
 
+// PASSEPORT CONFIG STRATEGY
+passport.use(new LocalStrategy(
+  {
+      usernameField: 'email',
+      passwordField: 'password',
+      session: false
+  },
+  function (email, password, cb) {
+    console.log('hello toto')
+    connection.query('SELECT iduser, email, password FROM users WHERE email = ?', email , function (err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (bcrypt.compareSync(password, user[0].password)!=true) { return cb(null, false); }
+      return cb(null, user);
+  })
+})
+)
+
+passport.use(new JwtStrategy({
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: key
+},
+function (jwtPayload, cb) {
+  console.log(jwtPayload)
+  return cb(null, jwtPayload);
+}
+));
+
+// LOGIN & TOKEN
+
+app.post('/api/login', function(req, res)  {
+  passport.authenticate('local',(err, users, info) => {
+    if(err)
+      return res.status(500).send(err)
+    if (!users)
+      return res.status(400).json({flash: 'erreur de login'});
+
+    const {iduser} = users[0];
+    const token = jwt.sign({iduser}, key, {expiresIn: 60*60});
+    console.log(token)
+    return res.json({
+      user: {userID},
+      token
+    })
+ })(req, res)
+})
+
 
 // TEST
 app.listen(port, (err) => {
@@ -28,4 +74,4 @@ app.listen(port, (err) => {
     throw new Error('Something bad happened...');
 	}
 	console.log(`Server is listening on ${port}`);
-});
+}); 
